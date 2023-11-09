44fbee4a4149e4b34c1bd34be1fd87ff23d1f8ee
FriiedToast <realatticl@gmail.com>
2023-11-09 22:45:43 +0100

    Update V1.1, Kick Command with Embed Log

--- bot.py
+++ bot.py
@@ -1,24 +1,35 @@
 import discord
-import asyncio
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import app_commands
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_API_TOKEN")
 
-intents = discord.Intents.all()
-intents.members = True
-bot = commands.Bot(command_prefix="kkk.", intents=intents,  help_command=None)
+bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
 
-async def load():
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
-        
-async def main():
-    await load()
-    await bot.start(TOKEN)
+@bot.event
+async def on_ready():
+    print("Bot is online!")
 
-asyncio.run(main())
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member:discord.Member, *, reason=None):
+    mod_logs = bot.get_channel(1172286709480828958)
+    if reason == None:
+        reason="No Reason Provided."
+    await ctx.guild.kick(member)
+    await ctx.send(f"User {member.mention} has been kicked for {reason}")
+
+    embed = discord.Embed(title=":athletic_shoe: Kick Log", colour=discord.Colour.random())
+    embed.set_author(name=f"{ctx.message.author.name}", icon_url=f"{ctx.message.author.display_avatar}")
+    embed.add_field(name="Member ", value=f"{member.name}")
+    embed.add_field(name="Reason ", value=f"{reason}")
+
+    if not mod_logs:
+        return 
+    await mod_logs.send(embed=embed) ##f"User {member.mention} has been kicked for {reason} by {ctx.message.author.mention}")
+
+bot.run(TOKEN)
